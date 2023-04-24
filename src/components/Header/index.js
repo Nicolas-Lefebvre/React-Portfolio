7f41214c536c8350cb900cc79a6bf7cce7508103
Nicolas-Lefebvre <nicolas.ap.lefebvre@gmail.com>
2023-04-24 10:54:56 +0200

    Modifs css header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,7 +17,7 @@ function Header() {
           <div>
             <a className="header-btn" href="#navbar">
               <span className="header-button">
-                Voir plus
+                découvrir
                 <i className="fa fa-arrow-right fa-1x" aria-hidden="true" />
               </span>
             </a>
