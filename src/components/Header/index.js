e5fe0059d66480fa14e89a28db145748173b4af5
Nicolas <nicolas@MacBook-Pro-de-Maya.local>
2024-01-06 22:39:38 +0100

    Modifs bouton header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 // == Import
 import './header.scss';
 import logo from '../../assets/images/logo-nicolas-lefebvre-nldev.png';
+import fleche from '../../assets/images/fleche-droite.png';
 
 // == Composant
 function Header() {
@@ -9,17 +10,19 @@ function Header() {
 
       <div className="header-wrapper" id="home">
         <div className="title-wrapper">
-          <img src={logo} alt="logo Nicolas Lefebvre nl-dev creation de sites web" />
+          <img className="logo" src={logo} alt="logo Nicolas Lefebvre nl-dev creation de sites web" />
           <h1>Nicolas Lefebvre</h1>
           <p>Création de sites web</p>
 
           {/* -------------------------------------BOUTON HEADER-------------------------------- */}
           <div>
             <a className="header-btn" href="#navbar">
-              <span className="header-button">
+              <button className="header-button" type="button">
                 découvrir
-                <i className="fa fa-arrow-right fa-1x" aria-hidden="true" />
-              </span>
+                {/* <i className="fa fa-arrow-right fa-1x" aria-hidden="true" /> */}
+                {/* <i className="fa-light fa-arrow-right" aria-hidden="true" /> */}
+                <img className='fleche' src={fleche} />
+              </button>
             </a>
           </div>
           {/* ---------------------------------------------------------------------------------- */}
